f99173dd55668fb5e0e1e156011c2a9bb3af6262
Micky92100 <mikaidoumbo@hotmail.fr>
2020-02-23 11:58:27 +0100

    dbase
    sql

--- ressources/db/projet_switch.sql
+++ ressources/db/projet_switch.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 19, 2020 at 12:26 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.1
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 23 fév. 2020 à 10:56
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,45 +19,50 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `projet_switch`
+-- Base de données :  `projet_switch`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `avis`
+-- Structure de la table `avis`
 --
 
-CREATE TABLE `avis` (
-  `id_avis` int(3) NOT NULL,
+DROP TABLE IF EXISTS `avis`;
+CREATE TABLE IF NOT EXISTS `avis` (
+  `id_avis` int(3) NOT NULL AUTO_INCREMENT,
   `id_membre` int(3) NOT NULL,
   `id_salle` int(3) NOT NULL,
   `commentaire` text COLLATE utf8_bin NOT NULL,
   `note` int(2) NOT NULL,
-  `date_enregistrement` datetime NOT NULL
+  `date_enregistrement` datetime NOT NULL,
+  PRIMARY KEY (`id_avis`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `commande`
+-- Structure de la table `commande`
 --
 
-CREATE TABLE `commande` (
-  `id_commande` int(3) NOT NULL,
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id_commande` int(3) NOT NULL AUTO_INCREMENT,
   `id_membre` int(3) NOT NULL,
   `id_produit` int(3) NOT NULL,
-  `date_enregistrement` datetime NOT NULL
+  `date_enregistrement` datetime NOT NULL,
+  PRIMARY KEY (`id_commande`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `membre`
+-- Structure de la table `membre`
 --
 
-CREATE TABLE `membre` (
-  `id_membre` int(3) NOT NULL,
+DROP TABLE IF EXISTS `membre`;
+CREATE TABLE IF NOT EXISTS `membre` (
+  `id_membre` int(3) NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(20) COLLATE utf8_bin NOT NULL,
   `mdp` varchar(60) COLLATE utf8_bin NOT NULL,
   `nom` varchar(20) COLLATE utf8_bin NOT NULL,
@@ -65,40 +70,43 @@ CREATE TABLE `membre` (
   `email` varchar(50) COLLATE utf8_bin NOT NULL,
   `civilite` enum('m','f') COLLATE utf8_bin NOT NULL,
   `statut` int(1) NOT NULL,
-  `date_enregistrement` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `date_enregistrement` datetime NOT NULL,
+  PRIMARY KEY (`id_membre`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `membre`
+-- Déchargement des données de la table `membre`
 --
 
 INSERT INTO `membre` (`id_membre`, `pseudo`, `mdp`, `nom`, `prenom`, `email`, `civilite`, `statut`, `date_enregistrement`) VALUES
-(1, 'ccqqccq', '$2y$10$ZQ1gxz5QKyYtAFAyK90kBuRkGnXl8VaMKPUoKU5tkteoI/uHoH0xq', 'cqcqcqc', 'qcqcqc', 'cqcqcqcqcc', 'm', 1, '2020-02-18 12:28:44'),
-(2, 'fsghhdh', '$2y$10$5tJsKA2IsczwPYq137uLG.og/xYI5BTXfVQj2k1mVDYS8uIPWo5K.', 'cqcqcqc', 'qcqcqc', 'cqcqcqcqcc', 'm', 1, '2020-02-18 12:28:58');
+(3, 'agent007', '$2y$10$f6QBlF10HL/NqTCemQmJ8uDJkJK3ypPEfygduYKp2WFR0Hb151PSa', 'bond', 'james', 'rggg@.fr', 'm', 1, '2020-02-20 23:38:21');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `produit`
+-- Structure de la table `produit`
 --
 
-CREATE TABLE `produit` (
-  `id_produit` int(3) NOT NULL,
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE IF NOT EXISTS `produit` (
+  `id_produit` int(3) NOT NULL AUTO_INCREMENT,
   `id_salle` int(3) NOT NULL,
   `date_arrivee` datetime NOT NULL,
   `date_depart` datetime NOT NULL,
   `prix` int(3) NOT NULL,
-  `etat` enum('libre','reservation') COLLATE utf8_bin NOT NULL
+  `etat` enum('libre','reservation') COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_produit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `salle`
+-- Structure de la table `salle`
 --
 
-CREATE TABLE `salle` (
-  `id_salle` int(3) NOT NULL,
+DROP TABLE IF EXISTS `salle`;
+CREATE TABLE IF NOT EXISTS `salle` (
+  `id_salle` int(3) NOT NULL AUTO_INCREMENT,
   `titre` varchar(200) COLLATE utf8_bin NOT NULL,
   `description` text COLLATE utf8_bin NOT NULL,
   `photo` varchar(200) COLLATE utf8_bin DEFAULT NULL,
@@ -107,90 +115,22 @@ CREATE TABLE `salle` (
   `adresse` varchar(50) COLLATE utf8_bin NOT NULL,
   `cp` int(5) NOT NULL,
   `capacite` int(3) NOT NULL,
-  `categorie` enum('reunion','bureau','formation') COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `categorie` enum('reunion','bureau','formation') COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_salle`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `salle`
+-- Déchargement des données de la table `salle`
 --
 
 INSERT INTO `salle` (`id_salle`, `titre`, `description`, `photo`, `pays`, `ville`, `adresse`, `cp`, `capacite`, `categorie`) VALUES
-(1, 'salle cézanne', 'Belle salle vue sur la mer', 'salle1.jpg', 'france', 'paris', '2 rue de paradis', 75015, 50, 'reunion'),
-(2, 'salle mozart', 'Magnifique salle vue sur les égouts de marseille', 'salle2.jpg', 'france', 'marseille', '5 rue de la plage', 13002, 10, 'bureau'),
-(3, 'salle basquiast', 'Superbe salle optionnelle de 500m2 ', 'salle3.jpg', 'france', 'Lyon', '45 rue de gerland', 69006, 100, 'reunion'),
-(4, 'ùklù', 'ùkùùùlk', 'salle4.jpg', 'France', 'Paris', '^gfjhgjjk', 78999, 1, 'reunion'),
-(5, 'xxxx', 'qqqqq', 'salle5.jpg', 'France', 'Paris', '2 rue antonie richard', 78000, 1, 'reunion'),
+(5, 'tttt', '9999', 'salle7.jpg', 'France', 'Paris', '45 avenu de rien', 93200, 1, 'reunion'),
 (6, 'fazeuohbdfuiazo', 'zdadda', 'salle6.jpg', 'France', 'Paris', 'dzadazd', 78000, 50, 'bureau'),
 (7, 'my room', 'its my room', 'salle7.jpg', 'France', 'Paris', 'my address', 78000, 50, 'bureau'),
-(8, 'test', 'test', 'salle8.jpg', 'France', 'Lyon', 'test', 87965, 1, 'reunion');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `avis`
---
-ALTER TABLE `avis`
-  ADD PRIMARY KEY (`id_avis`);
-
---
--- Indexes for table `commande`
---
-ALTER TABLE `commande`
-  ADD PRIMARY KEY (`id_commande`);
-
---
--- Indexes for table `membre`
---
-ALTER TABLE `membre`
-  ADD PRIMARY KEY (`id_membre`);
-
---
--- Indexes for table `produit`
---
-ALTER TABLE `produit`
-  ADD PRIMARY KEY (`id_produit`);
-
---
--- Indexes for table `salle`
---
-ALTER TABLE `salle`
-  ADD PRIMARY KEY (`id_salle`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `avis`
---
-ALTER TABLE `avis`
-  MODIFY `id_avis` int(3) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `commande`
---
-ALTER TABLE `commande`
-  MODIFY `id_commande` int(3) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `membre`
---
-ALTER TABLE `membre`
-  MODIFY `id_membre` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `produit`
---
-ALTER TABLE `produit`
-  MODIFY `id_produit` int(3) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `salle`
---
-ALTER TABLE `salle`
-  MODIFY `id_salle` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+(8, 'test', 'test', 'salle8.jpg', 'France', 'Lyon', 'test', 87965, 1, 'reunion'),
+(11, 'salle de versailles', 'belle et grande', 'salle2.jpg', 'France', 'Paris', '15 bd du roi', 78888, 50, 'reunion'),
+(12, 'dsffdsv', 'vsvfsvs', 'salle6.jpg', 'France', 'Marseille', 'bfdbd', 20202, 50, 'bureau'),
+(15, 'salle du lyon', 'au stade gerland', 'salle9.jpg', 'France', 'Lyon', '156 rue des perdants', 69200, 50, 'formation');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
