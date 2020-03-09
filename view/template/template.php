1100f3b66d523649b736a1a9db0b80f98a356be0
Alexis DRAI <drai.alexis@gmail.com>
2020-03-09 21:31:19 +0100

    various

--- view/template/template.php
+++ view/template/template.php
@@ -134,11 +134,4 @@ var_dump($_SESSION); ?>
 <!------------>
 <!-- FOOTER -->
 <!------------>
-
 </html>
-
-
-<!--TODO is that useful here?-->
-<!--    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"-->
-<!--            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"-->
-<!--            crossorigin="anonymous"></script>-->
