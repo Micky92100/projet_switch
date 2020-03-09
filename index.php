eb0bae7587b6b3e15d4227ad27ff6c27cbddd5cf
Alexis DRAI <drai.alexis@gmail.com>
2020-03-09 18:46:30 +0100

    cleaned up the router

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require('controller/controller.php');
 
 if (isset($_GET['action'])) {
+        ////////////////////////////////////////////////////////////////// ROOMS v
     if ($_GET['action'] == 'editRoom' && !empty($_GET['room-id'])) {
         if (
             isset($_POST['room-id']) &&
@@ -23,27 +24,29 @@ if (isset($_GET['action'])) {
         getDeleteRoom();
     } else if ($_GET['action'] == 'listRooms') {
         listRooms();
+        ////////////////////////////////////////////////////////////////// ROOMS ^
+        ////////////////////////////////////////////////////////////////// USERS v
     } else if ($_GET['action'] == 'editUser' && !empty($_GET['user-id'])) {
-            if (
-                isset($_POST['id_membre']) &&
-                isset($_POST['pseudo']) &&
-                isset($_POST['mdp']) &&
-                isset($_POST['nom']) &&
-                isset($_POST['prenom']) &&
-                isset($_POST['email']) &&
-                isset($_POST['civilite']) &&
-                isset($_POST['statut'])
-            ) {
-                createUser();
-            } else {
-                showUser($_GET['user-id']);
-            }
+        if (
+            isset($_POST['id_membre']) &&
+            isset($_POST['pseudo']) &&
+            isset($_POST['mdp']) &&
+            isset($_POST['nom']) &&
+            isset($_POST['prenom']) &&
+            isset($_POST['email']) &&
+            isset($_POST['civilite']) &&
+            isset($_POST['statut'])
+        ) {
+            createUser();
+        } else {
+            showUser($_GET['user-id']);
+        }
     } else if ($_GET['action'] == 'deleteUser' && !empty($_GET['user-id'])) {
         getDeleteUser();
     } else if ($_GET['action'] == 'listUsers') {
         listUsers();
-
-
+        ////////////////////////////////////////////////////////////////// USERS ^
+        ////////////////////////////////////////////////////////////////// PRODUCTS v
     } else if ($_GET['action'] == 'searchProducts') {
         if (isset($_POST['category']) &&
             isset($_POST['city']) &&
@@ -53,19 +56,7 @@ if (isset($_GET['action'])) {
             isset($_POST['departure'])) {
             searchProducts();
         }
-    } else if ($_GET['action'] == 'deconnexion') {
-        if (!isset($_SESSION)) {
-            session_start();
-        }
-        session_destroy();
-        header("Location: " . $_SERVER['PHP_SELF']);
-        getLogin();
-    } else if ($_GET['action'] == 'listOrders') {
-        showOrders();
-    } else if ($_GET['action'] == 'deleteOrder') {
-        getDeleteOrder();
-    }
-    if ($_GET['action'] == 'editProduct') {
+    } else if ($_GET['action'] == 'editProduct') {
         if (
             isset($_POST['arrival']) &&
             isset($_POST['departure']) &&
@@ -73,18 +64,28 @@ if (isset($_GET['action'])) {
             isset($_POST['price'])
         ) {
             saveProduct();
-        } else if (!empty($_GET['product-id'])){
+        } else if (!empty($_GET['product-id'])) {
             showProduct($_GET['product-id'], $_GET['room-id']);
         }
     } else if ($_GET['action'] == 'deleteProduct' && !empty($_GET['product-id'])) {
         getDeleteProduct();
     } else if ($_GET['action'] == 'listProducts') {
         listProducts();
+        ////////////////////////////////////////////////////////////////// PRODUCTS ^
+        ////////////////////////////////////////////////////////////////// ORDERS v
+    } else if ($_GET['action'] == 'listOrders') {
+        showOrders();
+    } else if ($_GET['action'] == 'deleteOrder') {
+        getDeleteOrder();
+        ////////////////////////////////////////////////////////////////// ORDERS ^
+        ////////////////////////////////////////////////////////////////// RATINGS v
+    } else if ($_GET['action'] == 'noticeList') {
+        showRates();
+        ////////////////////////////////////////////////////////////////// RATINGS ^
+        ////////////////////////////////////////////////////////////////// LOG&SIGN v
     } else if ($_GET['action'] == 'login') {
         header("Location: " . $_SERVER['PHP_SELF']);
         getLogin();
-    } else if ($_GET['action'] == 'noticeList') {
-        showRates();
     } else if ($_GET['action'] == 'doLogin') {
         header("Location: " . $_SERVER['PHP_SELF']);
         if (
@@ -104,8 +105,15 @@ if (isset($_GET['action'])) {
         } else {
             getSignUp();
         }
-
+    } else if ($_GET['action'] == 'deconnexion') {
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+        session_destroy();
+        header("Location: " . $_SERVER['PHP_SELF']);
+        getLogin();
     }
+    ////////////////////////////////////////////////////////////////// LOG&SIGN ^
 } else if (user_is_connected()) {
     listProductsIndex();
 } else {
